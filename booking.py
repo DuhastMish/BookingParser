c24687cb7d92bc36fdef5313e4dc2b7465f315ac
Andrey Maksimov <fantoff2010@yandex.ru>
2020-09-06 16:15:00 +0300

    Added date_out input and date_in by default today.

--- booking.py
+++ booking.py
@@ -1,14 +1,15 @@
+from datetime import datetime  # noqa:D100
 
 
-def create_link():
+def create_link(date_in, date_out):
     """Создание ссылки для сбора данных."""
-    month = "09"
-    day = "15"
-    year = "2020"
-    out_month="09"
-    out_day = "17"
-    out_year = "2020"
-    count_people = "1"
+    month = date_in.month
+    day = date_in.day
+    year = date_in.year
+    out_month = date_out.month
+    out_day = date_out.day
+    out_year = date_out.year
+    count_people = 1
     city = "Kazan"
     country = "Russia"
     off_set = 1000
@@ -39,7 +40,11 @@ def create_link():
 
 
 def main():
-    url = create_link()
+    """Главный метод по обработке данных."""
+    date_in = datetime.now()
+    print("Введите дату отьезда в формате ##.##.##")
+    date_out = datetime.strptime(input(), '%d.%m.%y')
+    url = create_link(date_in, date_out)
     print(url)
 
 
