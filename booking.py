44fb55283e88f971c647fc34ca73351679f80724
Andrey Maksimov <fantoff2010@yandex.ru>
2020-09-07 00:03:14 +0300

    Добавлен базовый метод для получения данных. Изменен ввод даты отьезда.

--- booking.py
+++ booking.py
@@ -1,7 +1,14 @@
 import datetime  # noqa:D100
+import requests
+from bs4 import BeautifulSoup
+
+session = requests.Session()
+REQUEST_HEADER = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36"}
+TODAY = datetime.datetime.now()
+NEXT_WEEK = TODAY + datetime.timedelta(7)
 
 
-def create_link(date_in: datetime.datetime, date_out: datetime.datetime):
+def create_link(date_in: datetime.datetime, date_out: datetime.datetime = NEXT_WEEK):
     """Создание ссылки для сбора данных."""
     month = date_in.month
     day = date_in.day
@@ -33,17 +40,30 @@ def create_link(date_in: datetime.datetime, date_out: datetime.datetime):
             country=country,
             limit=off_set
             )
-
+    print("Url created:" + "\n" + "\t" + url)
     return url
+
+
+def get_info(url: str):
+    """Получает данные по ссылке."""
+    response = session.get(url, headers=REQUEST_HEADER)
+    soup = BeautifulSoup(response.text, "lxml")
+    print("Начинаю собирать данные...")
+    import pdb; pdb.set_trace()
 
 
 def main():
     """Главный метод по обработке данных."""
-    date_in = datetime.datetime.now()
+    date_in = TODAY
     print("Введите дату отьезда в формате ##.##.##")
-    date_out = datetime.datetime.strptime(input(), '%d.%m.%y')
-    url = create_link(date_in, date_out)
-    print(url)
+    date_out = input()
+    if date_out:
+        date_out = datetime.datetime.strptime(date_out, '%d.%m.%y')
+        url = create_link(date_in, date_out)
+    else:
+        url = create_link(date_in)
+
+    get_info(url)
 
 
 if __name__ == "__main__":
