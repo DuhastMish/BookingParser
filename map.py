b6dc102bcf1d5a2133c770d03f9161b72747bca6
Vladislav <vladisloove.kondakov@gmail.com>
2020-09-24 19:20:47 +0300

    docs: add gmplot to requirements; fix: draw method now displays all hotels, delete unused import, add types to return values

--- map.py
+++ map.py
@@ -1,30 +1,29 @@
 import gmplot
-import datetime
+from typing import List
 
-# Получаем координаты для отелей 
-def get_coords(hotels_info: list):
+def get_coords(hotels_info: list) -> list:
+    # Получаем координаты для отелей 
 
     hotels_coordinates = []
 
     for page in range(len(hotels_info)):
         for hotel in range(len(hotels_info[page])):
-            coords = {
-                'latitude': hotels_info[page][hotel]['details']['coordinates']['latitude'],
-                'longitude': hotels_info[page][hotel]['details']['coordinates']['longitude'],
-            }
             hotels_coordinates.append({
                 'name': hotels_info[page][hotel]['name'],
-                'coordinates': coords,
+                'coordinates': {
+                    'latitude': hotels_info[page][hotel]['details']['coordinates']['latitude'],
+                    'longitude': hotels_info[page][hotel]['details']['coordinates']['longitude'],
+                },
             })
 
     return hotels_coordinates
 
-# Пример использования google maps
-def draw_map_by_coords(coords: list, task: str):
+def draw_map_by_coords(coords: list, task: str) -> None:
+    # Рисует карту с метками по заданным координатам
 
     gmap = gmplot.GoogleMapPlotter(coords[0]['coordinates']['latitude'], coords[0]['coordinates']['latitude'], 5)
-
-    for el in coords[:10]:
+    
+    for el in coords:
         ltd = float(el['coordinates']['latitude'])
         lgd = float(el['coordinates']['longitude'])
         gmap.marker(ltd, lgd)
