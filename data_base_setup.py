f920a2bf662ca248081ef4c82a2bc6dd5d820765
denisovalia <pletnevajulia50905@gmail.com>
2020-10-12 19:29:30 +0300

    исправила, что сказал хозяин

--- data_base_setup.py
+++ data_base_setup.py
@@ -1,8 +1,8 @@
-import sys
+
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
+
 
 Base = declarative_base()
 #здесь создадим классы
@@ -18,34 +18,35 @@ class hotels(Base):
 
 class details(hotels):
     _tablename_ = 'details'
+    details_id = Column(Integer, ForeignKey('hotels.hotels_id'))
 
 class coordinates(details):    
     _tablename_ = 'coordinates'
+    coordinates_id = Column(Integer, ForeignKey('details.details_id'))
 
     latitude = Column(String(100), nullable = False)
     longitude = Column(String(100), nullable = False)
 
 class important_facilities(details):
     _tablename_ = 'important_facilities'
+    important_facilities_id = Column(Integer, ForeignKey('details.details_id'))
 
     important_facilities= Column(String(100), nullable = False)
 
 class neighborhood_structures(details):
     _tablename_ = 'neighborhood_structures'
+    neighborhood_structures_id = Column(Integer, ForeignKey('details.details_id'))
     neighborhood_structures = Column(String(100), nullable = True)
 
 class services_offered(details):
     _tablename_ = 'services_offered'
-class type(services_offered):
-    _tablename_ = 'type'
+    services_offered_id = Column(Integer, ForeignKey('details.details_id'))
     services_offered = Column(String(100), nullable = False)
+    value = Column(String(1000), nullable = False)
 
-class value(services_offered):
-    _tablename_ = 'value'
-    value = Column(String(100), nullable = False)
     
 
 
 
-engine = create_engine('Y-%m-%d-%H.%M.%S')
+engine = create_engine('sqlite:///hotels.db', echo=True)
 Base.metadata.create_all(engine)
