a0236f2b4b32c62c78fe449242b95df37c05f5ed
Andrey Maksimov <fantoff2010@yandex.ru>
2020-11-01 00:02:31 +0300

    Litle fixes

--- data_base_setup.py
+++ data_base_setup.py
@@ -1,11 +1,10 @@
 
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 
 
 Base = declarative_base()
-#здесь создадим классы
+
 
 class hotels(Base):
     _tablename_ = 'HOTELS'
@@ -16,36 +15,38 @@ class hotels(Base):
     image = Column(String(100), nullable=False)
     link = Column(String(100), nullable=False)
 
+
 class details(hotels):
     _tablename_ = 'details'
     details_id = Column(Integer, ForeignKey('hotels.hotels_id'))
 
-class coordinates(details):    
+
+class coordinates(details):
     _tablename_ = 'coordinates'
     coordinates_id = Column(Integer, ForeignKey('details.details_id'))
 
-    latitude = Column(String(100), nullable = False)
-    longitude = Column(String(100), nullable = False)
+    latitude = Column(String(100), nullable=False)
+    longitude = Column(String(100), nullable=False)
+
 
 class important_facilities(details):
     _tablename_ = 'important_facilities'
     important_facilities_id = Column(Integer, ForeignKey('details.details_id'))
 
-    important_facilities= Column(String(100), nullable = False)
+    important_facilities = Column(String(100), nullable=False)
+
 
 class neighborhood_structures(details):
     _tablename_ = 'neighborhood_structures'
     neighborhood_structures_id = Column(Integer, ForeignKey('details.details_id'))
-    neighborhood_structures = Column(String(100), nullable = True)
+    neighborhood_structures = Column(String(100), nullable=True)
+
 
 class services_offered(details):
     _tablename_ = 'services_offered'
     services_offered_id = Column(Integer, ForeignKey('details.details_id'))
-    services_offered = Column(String(100), nullable = False)
-    value = Column(String(1000), nullable = False)
-
-    
-
+    services_offered = Column(String(100), nullable=False)
+    value = Column(String(1000), nullable=False)
 
 
 engine = create_engine('sqlite:///hotels.db', echo=True)
