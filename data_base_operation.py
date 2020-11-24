0fcc29311c244e9643057ba8e9d84759df4a46b3
fantoff2010@yandex.ru <fantoff2010@yandex.ru>
2020-11-24 12:59:46 +0300

    Removed extra =

--- data_base_operation.py
+++ data_base_operation.py
@@ -44,7 +44,7 @@ def is_hotel_exist(name: str, city: str, open_date: str) -> bool:
     existing = ()
     with DATABASE.begin() as connection:
         hotel = connection.execute(f"SELECT EXISTS(SELECT * FROM hotels WHERE
-                                   name == '{name}' AND city === '{city}' AND open_date == '{open_date}'")
+                                   name == '{name}' AND city == '{city}' AND open_date == '{open_date}'")
         existing = hotel.fetchone()
 
     return True if existing[0] == 1 else False
